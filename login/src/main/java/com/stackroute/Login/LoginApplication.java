27e909d0aca0d8df3a88670ec9ec24109faf08b5
Kgharte <ghartekalpesh10@gmail.com>
2019-02-12 22:45:11 +0530

    loginJWT added

---
+++ login/src/main/java/com/stackroute/Login/LoginApplication.java
@@ -0,0 +1,25 @@
+package com.stackroute.Login;
+
+import com.stackroute.Login.config.JwtFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LoginApplication {
+
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/api/v1/*");
+		return registrationBean;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
+

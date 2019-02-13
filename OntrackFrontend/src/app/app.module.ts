be5a1e749fb2e79b773e48e4050f1fb422d10bd4
Bharath <bharas21@in.ibm.com>
2019-02-13 16:13:50 +0530

    fianl dialog box

--- OntrackFrontend/src/app/app.module.ts
+++ OntrackFrontend/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OntrackModule } from './ontrack/ontrack.module';
-<<<<<<< HEAD
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { OntrackserviceService } from './ontrack/ontrackservice.service';
@@ -15,29 +13,25 @@ import { OntrackserviceService } from './ontrack/ontrackservice.service';
 // import { ChildregcompComponent } from './ontrack/components/regcomp/childregcomp/childregcomp.component';
 
 
->>>>>>> 287d03ec9dc1d328c335c6d13801301a9b5393f8
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-=======
+
     // RegcompComponent,
     // ChildregcompComponent
->>>>>>> 287d03ec9dc1d328c335c6d13801301a9b5393f8
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     OntrackModule,
-<<<<<<< HEAD
-    MatDialogModule,
-=======
+
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule
->>>>>>> 287d03ec9dc1d328c335c6d13801301a9b5393f8
+
   ],
   providers: [OntrackserviceService],
   bootstrap: [AppComponent]

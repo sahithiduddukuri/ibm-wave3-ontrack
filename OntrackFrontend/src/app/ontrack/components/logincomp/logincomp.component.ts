ff4a028e4604bf440c79d3dca90de4a112fcc598
Kgharte <ghartekalpesh10@gmail.com>
2019-02-13 12:07:23 +0530

    login componentadded

---
+++ OntrackFrontend/src/app/ontrack/components/logincomp/logincomp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logincomp',
+  templateUrl: './logincomp.component.html',
+  styleUrls: ['./logincomp.component.scss']
+})
+export class LogincompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

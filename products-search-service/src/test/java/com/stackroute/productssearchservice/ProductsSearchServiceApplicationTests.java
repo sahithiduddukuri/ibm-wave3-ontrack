26b3483090b484159e810b20ca4e797e15ca44f2
Kgharte <42381614+Kgharte@users.noreply.github.com>
2019-02-26 23:46:51 +0530

    Update ProductsSearchServiceApplicationTests.java

--- products-search-service/src/test/java/com/stackroute/productssearchservice/ProductsSearchServiceApplicationTests.java
+++ products-search-service/src/test/java/com/stackroute/productssearchservice/ProductsSearchServiceApplicationTests.java
@@ -1,16 +1,16 @@
-package com.stackroute.productssearchservice;
+// package com.stackroute.productssearchservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ProductsSearchServiceApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class ProductsSearchServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
 
-}
+// }

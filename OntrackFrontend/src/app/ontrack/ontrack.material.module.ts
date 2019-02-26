884297733faee8dc744ff8d29f17c297662b7707
Bharath <bharas21@in.ibm.com>
2019-02-26 09:24:15 +0530

    login and registration decorated

--- OntrackFrontend/src/app/ontrack/ontrack.material.module.ts
+++ OntrackFrontend/src/app/ontrack/ontrack.material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule, MatGridListModule, MatNativeDateModule} from '@angular/material';
+import { MatGridListModule, MatNativeDateModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatTooltipModule} from '@angular/material/tooltip';
@@ -15,6 +15,7 @@ import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material';
 import { FormsModule, FormGroup } from '@angular/forms';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     declarations: [],

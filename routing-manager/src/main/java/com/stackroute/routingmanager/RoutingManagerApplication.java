a5352ea7c061928f95a472d3515516b41c02ecce
Rachan <rachkau1@in.ibm.com>
2019-02-27 16:41:44 +0530

    created new project routing-manager

---
+++ routing-manager/src/main/java/com/stackroute/routingmanager/RoutingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.routingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingManagerApplication.class, args);
+	}
+
+}

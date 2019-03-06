8dcb4a3be85dc6fb45a16a2798841915d0b4aa09
bharath941996 <45656586+bharath941996@users.noreply.github.com>
2019-03-06 18:20:53 +0530

    Update buy.component.ts

--- OntrackFrontend/src/app/ontrack/components/buy/buy.component.ts
+++ OntrackFrontend/src/app/ontrack/components/buy/buy.component.ts
@@ -12,39 +12,14 @@ import { OnTrackService } from '../../services/ontrack.service';
   styleUrls: ['./buy.component.scss']
 })
 export class BuyComponent implements OnInit {
-  // myControl = new FormControl();
-  // options: string[] = ['Today', 'Tommorow', 'Day After Tommorow'];
-  // filteredOptions: Observable<string[]>;
-  // items: Array<any> = [];
-  constructor(private route: Router, private onTrack: OnTrackService) {
-    // this.items = [
-    // {name : ' 28/02/2018 '},
-    // {name : ' 1/03/2019 '},
-    // {name : ' 2/03/2019 '}  ];
+  constructor(private route: Router, private onTrack: OnTrackService) {  
 }
-
 slotsAvailable: any = [];
-// slots: any = [];
-
-
   ngOnInit() {
     this.onTrack.Buy().subscribe(data => {
       console.log('data fetch from response', data);
       this.slotsAvailable = data;
-      // console.log('data from slot availability', this.slotsAvailable);
-      // console.log(this.slotsAvailable.slots);
-    });
-    // this.filteredOptions = this.myControl.valueChanges
-  //   .pipe(
-  //     startWith(''),
-  //     map(value => this._filter(value))
-  //   );
-  }
-  // private _filter(value: string): string[] {
-  //   const filterValue = value.toLowerCase();
-
-  //   return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  // }
+    }); 
 shopping() {
 this.route.navigate(['/' , 'shopping']);
 }

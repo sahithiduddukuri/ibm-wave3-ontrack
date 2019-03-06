06843a6175d6b11ebde87bd82c84a7bcf366775a
bharath941996 <45656586+bharath941996@users.noreply.github.com>
2019-03-06 18:33:55 +0530

    Update card.component.ts

--- OntrackFrontend/src/app/ontrack/components/card/card.component.ts
+++ OntrackFrontend/src/app/ontrack/components/card/card.component.ts
@@ -1,7 +1,6 @@
 import { AngularFireDatabase } from '@angular/fire/database';
 import { CartComponent } from './../cart/cart.component';
 import { Router } from '@angular/router';
-
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { DailogComponent } from './../dailog/dailog.component';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
@@ -15,21 +14,19 @@ import { Ontrack } from '../../classes/ontrack';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
   // tslint:disable-next-line:max-line-length
-  constructor(public dialog: MatDialog, private onTrack: OnTrackService, public route: Router, private searchService: SearchService, private db: AngularFireDatabase) { }
-
-  products: any = [];
+constructor(public dialog: MatDialog, private onTrack: OnTrackService, public route: Router, private searchService: SearchService, private db: AngularFireDatabase) { }
+products: any = [];
 productName: String;
 @Input() Ontrack: any;
- private ontracks: Ontrack[];
+private ontracks: Ontrack[];
   @Input()
   product: any;
   cart: any ;
   @Output()
    cartAddEvent = new EventEmitter<any>();
-  dialogResult: any;
-  AddToCart(product): void {
+   dialogResult: any;
+   AddToCart(product): void {
     console.log(product, 'click event call');
     this.cartAddEvent.emit(product);
     this.onTrack.cart = product;
@@ -38,12 +35,10 @@ productName: String;
     console.log('data value', data);
   });
   this.db.list('/products').push(product);
-
   }
   buys() {
     this.route.navigate([ '/', 'buys']);
   }
-
   openDialog(o): void {
     console.log(o, 'this is the data ');
    const dialogRef = this.dialog.open(DailogComponent,  {
@@ -53,7 +48,6 @@ productName: String;
       console.log('The dialog was closed');
       this.dialogResult = result;
     });
-
 }
 ngOnInit() {
         this.onTrack.Home().subscribe(data => {

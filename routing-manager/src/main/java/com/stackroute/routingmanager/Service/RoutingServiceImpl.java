50f710f2916da84526c307320b72f3ef663e281c
Rachan <rachkau1@in.ibm.com>
2019-03-08 18:10:00 +0530

    routing-manager updated

--- routing-manager/src/main/java/com/stackroute/routingmanager/Service/RoutingServiceImpl.java
+++ routing-manager/src/main/java/com/stackroute/routingmanager/Service/RoutingServiceImpl.java
@@ -23,7 +23,7 @@ public class RoutingServiceImpl implements RoutingService {
     }
 
 
-    @Override
+   /* @Override
     public String saveOrder(Order order) {
         Node node = new Node();
         node.setNodeId(order.getOrderId());
@@ -33,11 +33,10 @@ public class RoutingServiceImpl implements RoutingService {
         System.out.println("inside save order service");
         vehicle.addNode(node);
         OptimizedSolutionImpl optimizedSolution= new OptimizedSolutionImpl();
-
         String date= order.getOrderDate();
         int noofOrder;
 
-        if (routingRepository.existsById(order.getOrderDate())){
+        *//*if (routingRepository.existsById(order.getOrderDate())){
 
             System.out.println("inside If");
             System.out.println("inside the if ");
@@ -59,6 +58,8 @@ public class RoutingServiceImpl implements RoutingService {
                 node1.setDemand(order.getDemand());
                 nodeList.add(node1);
                 slot1.setNodeList(nodeList);
+                slots.add(slot1);
+                order1.get().setSlots(slots);
 
             }
             else {
@@ -74,6 +75,7 @@ public class RoutingServiceImpl implements RoutingService {
                         nodeList.add(node1);
                         slot.setNodeList(nodeList);
                         slot.setSlotType(order.getSlotType());
+                        slots.add(slot);
 
                     } else {
                         System.out.println("in else of slot check***********");
@@ -89,12 +91,12 @@ public class RoutingServiceImpl implements RoutingService {
                         slot1.setNodeList(nodeList);
 //                    slot.setSlotType(order.getSlotType());
                     }
-
+                    order1.get().setSlots(slots);
                 }
             }
 
 
-            order1.get().setSlots(slots);
+
             order1.get().setNoOfOrders(noofOrder);
             routingRepository.save(order1.get());
         }else{
@@ -119,14 +121,98 @@ public class RoutingServiceImpl implements RoutingService {
 
             saveOrder.setSlots(slotList);
             routingRepository.save(saveOrder);
+        }*//*
+        if (routingRepository.existsById(order.getOrderDate())) {
+            Optional<SaveOrder> order1 = routingRepository.findById(order.getOrderDate());
+            noofOrder = order1.get().getNoOfOrders()+1;
+            List<Slot> slots = order1.get().getSlots();
+
+
+
+
+
         }
 
-       /*if(routingRepository.findAll().isEmpty()){
+       *//*if(routingRepository.findAll().isEmpty()){
            Optional<SaveOrder> order1 =
-       }*/
+       }*//*
 
         optimizedSolution.optimizedCost(noofOrder,order.getX(),order.getY(),order.getDemand());
         //routingRepository.save(order);
+        return "order added";
+    }*/
+
+    @Override
+    public String saveOrder(Order order) {
+        Node node = new Node();
+        node.setNodeId(order.getOrderId());
+        node.setNode_X(order.getX());
+        node.setNode_Y(order.getY());
+        node.setDemand(order.getDemand());
+        System.out.println("inside save order service");
+        vehicle.addNode(node);
+        OptimizedSolutionImpl optimizedSolution = new OptimizedSolutionImpl();
+
+       // String date = order.getOrderDate();
+        int noofOrder;
+        SaveOrder saveOrder;
+        List<Node> nodeList;
+
+        if(routingRepository.existsById(order.getOrderDate()))
+        {
+            Optional<SaveOrder> saveOrder =  routingRepository.findById(order.getOrderDate());
+            saveOrder.get().setOrderDate(order.getOrderDate());
+            saveOrder.get().setVehicleId(order.getVehicleId());
+            saveOrder.get().setOrderId(order.getOrderId());
+            System.out.println("get order Date++@@@@####$$$$$$$$$$$$@@@@@@@@@@@@@@@@@######$$$$$"+saveOrder.get().getNoOfOrders());
+            noofOrder = saveOrder.get().getNoOfOrders()+1;
+            List<Slot> slots = saveOrder.get().getSlots();
+            System.out.println("Slots value is"+slots);
+            for(int i=0;i<slots.size();i++)
+            {
+                System.out.println("inside for loop");
+                if(slots.get(i).getSlotType().equals(order.getSlotType()))
+                {
+                    System.out.println("this type is equal"+slots.get(i).getSlotType());
+                    nodeList = slots.get(i).getNodeList();
+                    nodeList.add(node);
+                    slots.get(i).setNodeList(nodeList);
+                    System.out.println("this is after");
+                }
+            }
+            saveOrder.get().setSlots(slots);
+            routingRepository.save(saveOrder.get());
+        }
+        else{
+            System.out.println("inside else first time############################");
+            saveOrder= new SaveOrder();
+            noofOrder = 2;
+            saveOrder.setNoOfOrders(noofOrder);
+            saveOrder.setOrderDate(order.getOrderDate());
+            saveOrder.setOrderId(order.getOrderId());
+            newOrder.setVehicleId(order.getVehicleId());
+            nodeList = new ArrayList<>();
+            List<Slot> slots = new ArrayList<>();
+            Slot slot = new Slot("A", nodeList);
+            Slot slot1 = new Slot("B", nodeList);
+            Slot slot2 = new Slot("C", nodeList);
+            slots.add(slot);
+            slots.add(slot1);
+            slots.add(slot2);
+            System.out.println("slots are +++++"+slots.size());
+//            for(int i=0;i < slots.size();i++)
+//            {
+//                System.out.println("Slots value are+++++==========++++++++++++++++"+slots);
+//
+//            }
+            newOrder.setSlots(slots);
+            newOrder.setNoOfOrders(noofOrder);
+            System.out.println("Before Routing service save for new save");
+            routingRepository.save(newOrder);
+
+        }
+        optimizedSolution.optimizedCost(noofOrder, order.getX(), order.getY(), order.getDemand());
+       routingRepository.save(order);
         return "order added";
     }
 

5bf11784884f3a04b403e6e86ec62da4db879f2e
Rachan <rachkau1@in.ibm.com>
2019-03-14 09:03:47 +0530

    visualization added

--- DM-login-angular-master/src/app/chart/app.chart.ts
+++ DM-login-angular-master/src/app/chart/app.chart.ts
@@ -1,35 +1,49 @@
-import { Component, OnInit } from "@angular/core";
-import { Chart } from "chart.js/dist/Chart.bundle.js";
+import { Component } from '@angular/core';
+import { Chart } from 'chart.js/dist/Chart.bundle.js';
 import { ChartsService } from '../services/charts.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: "app-chart",
-  templateUrl: "./app.chart.html",
-  styleUrls: ["./app.chart.scss"]
+  selector: 'app-chart',
+  templateUrl: './app.chart.html',
+  styleUrls: ['./app.chart.scss']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent {
   chart = [];
+  date: any;
+  slot: String;
+  formatDate: any;
 
-  constructor(private chartsService: ChartsService) {}
+  constructor(private chartsService: ChartsService, private datePipe: DatePipe) {}
 
-  ngOnInit() {
-    this.getChartInfo();
-  }
-
-  getChartInfo() {
-    this.chartsService.getJSON("11-04-2019", "B").subscribe(res => {
-      console.log("res from json server", res);
+  // getSelectedSlot(value: String) {
+  //  console.log('this is the value of selected', value);
+  //  this.slot = value;
+  // }
+  getChartInfo(dateValue: Date, slotType: String) {
+    this.date = dateValue;
+    this.slot = slotType;
+    this.formatDate = this.datePipe.transform(dateValue, 'yyyy-MM-dd');
+    console.log('date ', dateValue);
+    console.log('slot type value', slotType);
+    console.log('Date after format', this.formatDate);
+    this.chartsService.getJSON(this.formatDate, this.slot).subscribe(res => {
+      console.log('res from json server', res);
         let lat;
         let long;
         let allNodes;
-      lat = res.map(e => {
-          return e.Route.map(f => {
+      lat = res.map(e => {  
+        return e.Route.map(f => {
+          if (f != null) {
           return f.node_X;
+          }
         });
       });
       long = res.map(e => {
         return e.Route.map(f => {
+          if (f != null) {
           return f.node_Y;
+          }
         });
       });
       allNodes = res.map(e => {
@@ -37,34 +51,30 @@ export class ChartComponent implements OnInit {
           return f.nodeId;
         });
       });
-      let nodes = [];
-      console.log(allNodes, lat, long);
-      allNodes.forEach((res: any) => {
-        nodes.push(res);
-      });
+      // let nodes = [];
+      // console.log(allNodes, lat, long);
+      // allNodes.forEach((res: any) => {
+      //   nodes.push(res);
+      // });
       this.newChart(lat[0], lat[0], long[0]);
     });
   }
 
   newChart(allNodes: any, lat: any, long: any) {
-    this.chart = new Chart("canvas", {
-      type: "line",
+    this.chart = new Chart('canvas', {
+      type: 'line',
       data: {
         labels: allNodes,
         datasets: [
           {
             data: lat,
-            borderColor: "#3cba9f",
+            borderColor: '#3cba9f',
             fill: false,
-            // label: "latitude",
-            // title: "latitude"
           },
           {
             data: long,
-            borderColor: "#ffcc00",
+            borderColor: '#ffcc00',
             fill: false,
-            // label: "longitude",
-            // title: "longitude"
           }
         ]
       },
--- DM-login-angular-master/src/app/services/charts.service.ts
+++ DM-login-angular-master/src/app/services/charts.service.ts
@@ -8,12 +8,15 @@ export class ChartsService {
 
   constructor(
     private http: HttpClient) {
-    this.getJSON("11-04-2019", "B").subscribe(data => {
-    });
   }
 
   getJSON(date, slot): Observable<any> {
 
-    return this.http.get('http://localhost:3001/route' + "?" + "date=" + date + "&" + "slotType=" + slot);
+    return this.http.get('http://localhost:8010/api/v1/route' + "?" + "date=" + date + "&" + "slotType=" + slot);
   }
 }
+
+
+
+
+

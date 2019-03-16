39e44d6ae6fc8299d496f0332c83c2777695333d
Rachan <rachkau1@in.ibm.com>
2019-03-17 00:49:04 +0530

    added buying history and card conponent

--- OntrackFrontend/src/app/ontrack/components/buying-history/buying-history.component.ts
+++ OntrackFrontend/src/app/ontrack/components/buying-history/buying-history.component.ts
@@ -1,10 +1,10 @@
 import { JwtHelperService } from '@auth0/angular-jwt';
-
+​
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { AngularFireDatabase, AngularFireObject} from 'angularfire2/database';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
-
+​
 @Component({
 selector: 'app-buying-history',
 templateUrl: './buying-history.component.html',
@@ -13,20 +13,20 @@ styleUrls: ['./buying-history.component.scss']
 export class BuyingHistoryComponent implements OnInit {
 products: any;
 dataProducts: any;
-
-
-constructor(private db: AngularFireDatabase, private route: Router) {  }
+​
+constructor(private db: AngularFireDatabase, private route: Router) { }
 private helper = new JwtHelperService();
 private currentUser;
 ngOnInit() {
-console.log('this is buying history');
-this.db.list('/products').valueChanges().subscribe(data => {
- console.log('data value from firebase', data);
- const pr = data;
- console.log('value of pr is ', pr);
- this.products = data;
- console.log('value in products', this.products);
-});
+ let itemRef = this.db.list('/products');
+  let items = itemRef.snapshotChanges().pipe(map(changes => changes.map(c => ({key: c.payload.key, ...c.payload.val()}))));
+​
+  items.subscribe(data => {
+  this.dataProducts = data;
+  this.products = data;
+  console.log('@@@@@@@^^^@@@@@@@', this.dataProducts);
+   console.log('in buying history$$$$$$$$$$$$$$$', data);
+  });
 console.log('data store in products', this.products);
 console.log('storage value is +++++of total', sessionStorage.setItem('count', this.products.length));
 this.currentUser = this.helper.decodeToken(localStorage.getItem('token'));
@@ -34,44 +34,22 @@ console.log(this.helper.decodeToken(localStorage.getItem('token')));
 }
 totalPrice() {
  let total = 0;
-  for (let count = 0; count < this.products.length; count++) {
-      total +=  Number(this.products[count].price);
-      console.log(total);
-      sessionStorage.setItem('price', total.toString());
-  }
-  return total;
+ for (let count = 0; count < this.products.length; count++) {
+   total += Number(this.products[count].price);
+   console.log(total);
+   sessionStorage.setItem('price', total.toString());
+ }
+ return total;
  }
 continue3() {
-  this.route.navigate(['/' , 'continue']);
+ this.route.navigate(['/' , 'continue']);
 }
 buy3() {
-  this.route.navigate(['/' , 'buy']);
+ this.route.navigate(['/' , 'buy']);
 }
-remove3(products_id) {
-//  this.db.list('/products').remove();
-//   console.log(this.db.list('/products', ref => ref.orderByChild('userName').equalTo(this.currentUser.jti)));
-//   console.log('removed');
-
-     let itemRef =  this.db.list('/products');
-     let items = itemRef.snapshotChanges().pipe( map(changes => changes.map(c => ({key: c.payload.key, ...c.payload.val()}))));
-     items.subscribe(data => {
-       console.log('@@@#####@@@@$$$$$$######@@@@@@$$$$%%%%%', data);
-     }); 
-    this.db.list('/products').valueChanges().subscribe(data => {
-      console.log('data value from firebase', data);
-      this.dataProducts = data;
-      this.dataProducts.map(resp => {
-         console.log('this is response ', resp);
-      });
-    });
-
-  // this.db.database().ref('.products');
-    console.log('this is the image url', products_id);
-    console.log('/products/' + products_id);
-    this.db.list('/products/' + products_id).remove();
-    this.db.list('/products/-L_voORmcalYxWvn5r7r').remove();
-
-    console.log(this.db.list('/products', ref => ref.orderByChild('userName').equalTo(this.currentUser.jti)));
-    console.log('removed');
-  }
+remove3(product) {
+  console.log('###########$$$######', product);
+  this.db.list('/products/' + product.key).remove();
+  console.log('**************');
+ }
 }
--- OntrackFrontend/src/app/ontrack/components/card/card.component.ts
+++ OntrackFrontend/src/app/ontrack/components/card/card.component.ts
@@ -7,6 +7,7 @@ import { OnTrackService } from '../../services/ontrack.service';
 import { SearchService } from '../../services/search.service';
 import { Ontrack } from '../../classes/ontrack';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { map  } from 'rxjs/operators';
 
 @Component({
   selector: 'app-card',
@@ -18,11 +19,13 @@ export class CardComponent implements OnInit {
 constructor(public dialog: MatDialog, private onTrack: OnTrackService, public route: Router, private searchService: SearchService, private db: AngularFireDatabase) { }
 products: any = [];
 productName: String;
+productsInfo: Ontrack;
 @Input() Ontrack: any;
-private ontracks: Ontrack[];
+private ontracks: Ontrack;
   @Input()
   product: any;
   cart: any ;
+  key: any;
   @Output()
    cartAddEvent = new EventEmitter<any>();
    dialogResult: any;
@@ -33,11 +36,39 @@ private ontracks: Ontrack[];
     this.cartAddEvent.emit(product);
     this.onTrack.cart = product;
    this.route.navigateByUrl('/AddToCart');
-   this.db.list('/products').valueChanges().subscribe(data => {
-    console.log('data value', data);
-  });
+   let itemRef = this.db.list('/products');
+   let items = itemRef.snapshotChanges().pipe(map(changes => changes.map(c => ({key: c.payload.key, ...c.payload.val()}))));
+  //  items.subscribe(console.log)
+   console.log('**************');
+   items.subscribe(data => {
+     console.log('data saved in firebased is******', data);
+     this.key = data;
+     this.ontracks = new Ontrack();
+     console.log('key value while pushing$$$$$$$$$$$$', this.key.map(key =>{
+       console.log('%%%%%%%%', key.key);
+       this.ontracks.key = key.key;
+       this.ontracks.brand = key.brand;
+       this.ontracks.brandId = key.brandId;
+       this.ontracks.colour = key.colour;
+       this.ontracks.description = key.description;
+       this.ontracks.dimension = key.dimension;
+       this.ontracks.gender = key.gender;
+       this.ontracks.imageURL = key.imageURL;
+       this.ontracks.mrp = key.mrp;
+       this.ontracks.price = key.price;
+       this.ontracks.productName = key.productName;
+       this.ontracks.productType = key.productType;
+       this.ontracks.weight = key.weight;
+      //    console.log('this is the value of track Service***&&&&&*******', this.ontracks);
+     }));
+   });
+  //  this.db.list('/products').valueChanges().subscribe(data => {
+  //   console.log('data value', data);
+  // });
+ 
   product.userName = this.currentUser.jti;
-  this.db.list('/products').push(product);
+   this.db.list('/products').push(product);
+  //  this.db.list('/productsList').push(this.ontracks);
   }
   buys() {
     this.route.navigate([ '/', 'buys']);

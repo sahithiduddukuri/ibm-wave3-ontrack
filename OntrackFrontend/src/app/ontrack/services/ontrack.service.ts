70041810c904a418a676323fbaaed45348fdc3be
Rachan <45649992+RachanSaini@users.noreply.github.com>
2019-03-18 08:06:14 +0530

    Update ontrack.service.ts

--- OntrackFrontend/src/app/ontrack/services/ontrack.service.ts
+++ OntrackFrontend/src/app/ontrack/services/ontrack.service.ts
@@ -22,17 +22,17 @@ export class OnTrackService {
     }
    Home() {
 
-    this.url = 'https://shoefly-zuul.stackroute.io/recommendation-service/api/v1/products';
+    this.url = 'https://shoefly-zuul.stackroute.io/product-service/api/v1/product';
     console.log('this method call');
     return this.http.get(this.url);
 }
 
 openDailog() {
-    this.url = 'https://shoefly-zuul.stackroute.io/recommendation-service/api/v1/products';
+    this.url = 'https://shoefly-zuul.stackroute.io/product-service/api/v1/product';
     return this.http.get(this.url);
 }
 AddToCart() {
-    this.url = 'https://shoefly-zuul.stackroute.io/recommendation-service/api/v1/products';
+    this.url = 'https://shoefly-zuul.stackroute.io/product-service/api/v1/product';
     return this.http.get(this.url);
 }
 openBottomSheet() {
@@ -52,7 +52,7 @@ profile() {
     return this.http.get(this.url);
 }
 REMOVE() {
-    this.url = 'https://shoefly-zuul.stackroute.io/recommendation-service/api/v1/AddToCart';
+    this.url = 'https://shoefly-zuul.stackroute.io/product-service/api/v1/product';
     return this.http.get(this.url);
  }
 
